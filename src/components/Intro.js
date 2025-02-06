326d08d13e7b4c3f65f9b242a70397123f321b8c
albatross-svg <gashutesfu@gmail.com>
2025-02-06 21:54:38 +0900

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -24,9 +24,7 @@ const Intro = () => {
 Our highly skilled team combines technical expertise with a deep understanding of client needs, enabling us to deliver solutions that are both innovative and dependable.   </p>
                             </div>
                             
-                            <div>
-                                <p className='my-3 text-xl text-gray-600 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id itaque quam aut maiores, sapiente consectetur. </p>
-                            </div>
+                           
                             <Link to="/contact" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-2 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0 group">
                                 Contact us
                                 <svg className="w-4 h-4 ml-1 group-hover: translate-x-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
