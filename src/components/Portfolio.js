fc5e5127fba1ae2c785214ebefa3a178dfbf0534
albatross-svg <gashutesfu@gmail.com>
2025-02-10 23:17:02 +0900

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,14 +2,7 @@ import React from 'react';
 
 const EssentialComponent = () => {
     return (
-        <>
-            <div className="my-4 py-4">
-                
-                <div className='flex justify-center'>
-                    <div className='w-24 border-b-4 border-blue-900 mb-8'></div>
-                </div>
-            </div>
-        </>
+        
     );
 };
 
