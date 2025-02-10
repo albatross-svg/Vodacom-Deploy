6e135124d0afef4a2796ab916c6bea407dc52f4c
albatross-svg <gashutesfu@gmail.com>
2025-02-11 02:29:34 +0900

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -43,7 +43,7 @@ const Contact = () => {
 
                     <a href="https://www.tiktok.com/@yourtiktok" target="_blank" rel="noreferrer">
                         <img
-                            src="https://upload.wikimedia.org/wikipedia/en/a/a9/TikTok_logo.svg" 
+                            src="https://upload.wikimedia.org/wikipedia/commons/6/69/TikTok_logo_without_text.svg"
                             alt="TikTok"
                             className="w-24 h-24 rounded-full shadow-lg hover:scale-110 transition-transform duration-300"
                         />
