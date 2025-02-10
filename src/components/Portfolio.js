dd390b99271af37b6b459f9b3087d411ae107b7c
albatross-svg <gashutesfu@gmail.com>
2025-02-10 23:15:07 +0900

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,9 +4,7 @@ const EssentialComponent = () => {
     return (
         <>
             <div className="my-4 py-4">
-                <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">
-                    Essential Section
-                </h2>
+                
                 <div className='flex justify-center'>
                     <div className='w-24 border-b-4 border-blue-900 mb-8'></div>
                 </div>
