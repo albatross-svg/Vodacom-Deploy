ce61effae82659f7a7be56eab4fd883b038164e4
albatross-svg <gashutesfu@gmail.com>
2025-02-14 20:46:03 +0900

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,14 +6,12 @@ import heroImg from '../images/web-dev.svg';
 const Hero = () => {
     return (
         <>
-            {/* Added extra padding to prevent overlap with the fixed navbar */}
-            <div className="hero pt-24 pb-12 md:pb-24" id="hero">
-                <div>
-                    <NavBar />
-                </div>
+            {/* Reduced padding-top to create proper spacing with navbar */}
+            <div className="hero pt-16 pb-12 md:pb-24" id="hero">
+                <NavBar />
 
-                {/* Added more top margin to separate Hero from Navbar in mobile */}
-                <div className="m-auto overflow-hidden mx-4 mt-12 lg:mt-4 p-2 md:p-12 h-5/6" data-aos="zoom-in">
+                {/* Adjusted margin to keep content properly spaced from navbar */}
+                <div className="m-auto overflow-hidden mx-4 mt-6 lg:mt-4 p-2 md:p-12 h-5/6" data-aos="zoom-in">
                     <div id="hero" className="flex flex-col lg:flex-row py-8 justify-between text-center lg:text-left">
                         <div className="lg:w-1/2 flex flex-col justify-center" data-aos="zoom-in" data-aos-delay="200">
                             <h1 className="mb-5 md:text-5xl text-3xl font-bold text-blue-900">
